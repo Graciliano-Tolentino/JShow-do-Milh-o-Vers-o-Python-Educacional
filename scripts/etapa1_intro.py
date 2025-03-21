206d0ad104f38aab81e4ddf82a681d802d2464a8
Graciliano-Tolentino <graciliano.tolentino@fatec.sp.gov.br>
2025-03-21 19:28:09 -0300

    Create etapa1_intro.py

---
+++ scripts/etapa1_intro.py
@@ -0,0 +1,8 @@
+def exibir_menu():
+    print("=" * 50)
+    print(" BEM-VINDO AO SHOW DO MILHÃO - VERSÃO PYTHON")
+    print("=" * 50)
+    print("Responda corretamente as perguntas e acumule pontos!")
+    print("Você terá 3 ajudas: Pular, Eliminar duas e Dica.")
+    print("Boa sorte!\n")
+    input("Pressione Enter para iniciar o jogo...")
